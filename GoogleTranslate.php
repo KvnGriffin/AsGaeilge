d17ddd93cc43032da8ff87dc80a182463cdebccf
KvnGriffin <kevingriffin79@gmail.com>
2012-04-02 17:15:39 +0200

    Update GoogleTranslate.php

--- GoogleTranslate.php
+++ GoogleTranslate.php
@@ -4,7 +4,7 @@
     {
 		/****************     Google Translate API    **************/
         $query = urlencode($query1);     
-        $google = file_get_contents("https://www.googleapis.com/language/translate/v2?key=AIzaSyDIoMMdU1TM_8pCtVK--TLjF08Nb2rWOao&source=en&target=ga&q=".$query."");
+        $google = file_get_contents("https://www.googleapis.com/language/translate/v2?key=**** YOUR API KEY HERE****&source=en&target=ga&q=".$query."");
         $decodegoogle = json_decode($google,TRUE); // TRUE for in array format
       
         foreach($decodegoogle['data']['translations'] as $decode) { // process JSON file
